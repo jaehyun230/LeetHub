ac13cb1263cbe912bba716deedfec3098222193c
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-07-17 23:32:20 +0900

    Time: 99 ms (75.21%), Space: 14.2 MB (28.75%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,19 +1,23 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
-        
         answer = []
         
-        def dfs(csum, start, use):
-            if csum < 0:
+        def dfs(sum_val, start, use_list) :
+            
+            if sum_val < 0 :
                 return
-            elif csum == 0:
-                answer.append(use)
+                
+            if sum_val == 0 :
+                answer.append(use_list)
                 return
-            # If no duplicates were allowed, dfs would pass i+1 instead
-            for i in range(start,len(candidates)):
-                dfs(csum-candidates[i], i, use+[candidates[i]])
-
-        dfs(target,0,[])
+            
+            elif sum_val > 0 :
+                for i in range(start, len(candidates)) :
+                    dfs(sum_val - candidates[i], i, use_list+ [candidates[i]])
+        
+        
+        dfs(target, 0, [])
         
         return answer
+                
