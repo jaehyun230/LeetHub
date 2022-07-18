63b489d74cf08854e8c2cdedd1fc926f732fbd03
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-07-18 14:52:01 +0900

    Time: 8979 ms (18.36%), Space: 15.2 MB (12.75%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -18,6 +18,10 @@ class Solution:
             cost, now = heapq.heappop(q)
             
             for i in range (1, nums[now]+1) :
+                
+                if now+i == len(nums)-1 :
+                    return cost+1
+                
                 if now+i < len(nums) and dp[now+i] > cost+1 :
                     heapq.heappush(q, (cost+1, now+i))
                     dp[now+i] = cost +1
