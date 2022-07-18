6c816d03342df674be611c485d08d7485a2f9fcc
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-07-18 11:29:52 +0900

    Time: 32 ms (97.25%), Space: 13.8 MB (74.17%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        
+        answer = int(num1) * int(num2)
+        
+        answer = str(answer)
+        
+        return answer
