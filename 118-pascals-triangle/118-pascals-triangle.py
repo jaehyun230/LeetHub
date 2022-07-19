25b3ce0314a3134c991afdfc5442fd5ac1f92dba
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-07-19 09:36:44 +0900

    Time: 43 ms (63.47%), Space: 13.8 MB (97.62%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -0,0 +1,23 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        answer = [[1]]
+        
+        print(len(answer[0]))
+        for i in range (numRows-1) :
+            temp = []
+            for j in range(len(answer[i])) :
+                if j == 0 :
+                    temp.append(1)
+                
+                else :
+                    value = answer[i][j-1] + answer[i][j]
+                    temp.append(value)
+                    
+                if j == len(answer[i]) - 1 :
+                    temp.append(1)
+                
+            answer.append(temp)
+        
+        
+        return answer
+        
