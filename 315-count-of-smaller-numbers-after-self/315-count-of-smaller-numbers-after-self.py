44accb99710fc2e575c8ce234b81a6b0e9e7eeaf
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-07-23 20:59:31 +0900

    Time: 2555 ms (91.72%), Space: 32.5 MB (81.15%) - LeetHub

---
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -0,0 +1,14 @@
+import bisect
+
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        answer =sorted(nums)
+        
+        result=[]
+        
+        for i in nums:
+            index = bisect.bisect_left(answer,i)
+            result.append(index)
+            del answer[index]
+            
+        return result
