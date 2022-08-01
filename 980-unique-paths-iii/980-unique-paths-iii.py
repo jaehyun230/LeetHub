8d1f39769ef495400b2439ee6163c4d742002fd4
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-08-01 12:59:49 +0900

    Time: 108 ms (38.68%), Space: 14 MB (54.71%) - LeetHub

--- 980-unique-paths-iii/980-unique-paths-iii.py
+++ 980-unique-paths-iii/980-unique-paths-iii.py
@@ -25,8 +25,6 @@ class Solution:
                     else :
                         dfs(mx, my, path + [[mx, my]])
             
-            
-        
         for i in range (len(grid)) :
             for j in range (len(grid[i])) :
                 if grid[i][j] == 0 :
