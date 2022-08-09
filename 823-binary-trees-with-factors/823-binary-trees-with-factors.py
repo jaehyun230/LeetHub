9b7e6411a879267c465b58c38b91d78eb5ae3dfb
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-08-09 10:20:53 +0900

    Time: 2799 ms (10.60%), Space: 16.7 MB (6.06%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.py
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.py
@@ -1,13 +1,28 @@
+from collections import defaultdict
+
 class Solution:
-    def numFactoredBinaryTrees(self, A: List[int]) -> int:
-        As = set(A) # set for O(1) lookup 
-        pq = [] # min heap 
-        for x, y in product(A, A): 
-            if x*y in As: heappush(pq, (x*y, x, y))
+    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
         
-        cnt = {x: 1 for x in A}
-        while pq: 
-            z, x, y = heappop(pq)
-            cnt[z] += cnt[x] * cnt[y]
+        #중복제거
+        dic = defaultdict(set)   
+        arr.sort()
         
-        return sum(cnt.values()) % 1_000_000_007
+        for i in range (len(arr)) :
+            for j in range (i) :
+                if arr[i] % arr[j] == 0 :
+                    res = arr[i]//arr[j]
+                    if res in arr :
+                        dic[i].add((j, arr.index(res)))
+        @cache
+        def dfs(i):
+            now = 1
+            for l, r in dic[i]: 
+                now += dfs(l) * dfs(r)
+            return now
+        
+        ans = 0           
+        for i in range(len(arr)):        
+            ans += dfs(i)        
+        return ans %(10**9 + 7)
+        
+            
