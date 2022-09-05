3ea32db26d0815e7ea33d436d22c5aa864bdc865
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-09-05 09:13:40 +0900

    Time: 70 ms (68.16%), Space: 16 MB (50.08%) - LeetHub

---
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+from collections import defaultdict
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        
+        answer = []
+        self.dic = defaultdict(list)
+        
+        def dfs(node, depth) :
+            
+            if not node :
+                return
+            self.dic[depth].append(node.val)
+            
+            if node.children :
+                for child in node.children :
+                    dfs(child, depth+1)
+        
+        dfs(root, 0)
+        
+        for i in self.dic :
+            answer.append(self.dic[i])
+        
+        return answer
