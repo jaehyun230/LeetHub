e6c714eb65fa8d972e685c15bd746dc56d6cf873
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-09-09 12:57:37 +0900

    Time: 5314 ms (5.04%), Space: 67.1 MB (79.12%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
+        properties.sort(key = lambda x: (-x[0], x[1]))
+        
+        mxattack=properties[0][0]
+        mxdefense=properties[0][1]
+        
+        answer=0
+        
+        for i in range(1,len(properties)):
+            if properties[i][0]<mxattack and properties[i][1]<mxdefense:
+                answer+=1
+            else:
+                mxattack=properties[i][0]
+                mxdefense=properties[i][1]
+        return answer
+        
