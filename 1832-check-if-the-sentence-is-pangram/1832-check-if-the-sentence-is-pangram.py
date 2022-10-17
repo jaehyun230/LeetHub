e2ec0d9cda65ea63ac01a8f8036db13c24b20c6f
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-10-17 09:10:16 +0900

    Time: 61 ms (36.98%), Space: 13.9 MB (54.73%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        dic = {}
+        for i in sentence :
+            if i not in dic :
+                dic[i] = 1
+        
+        if len(dic) == 26 :
+            return True
+        
+        return False
