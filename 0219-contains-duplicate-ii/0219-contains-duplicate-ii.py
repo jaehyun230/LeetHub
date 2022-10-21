5e3ae04643bc6919155c2164a4d4f96235a73391
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-10-21 09:54:51 +0900

    Time: 1786 ms (8.36%), Space: 27.3 MB (32.52%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -11,8 +11,7 @@ class Solution:
         for i in range(len(nums)) :
             
             if i <= k :
-                if nums[i] in dic :
-                    
+                if nums[i] in dic :     
                     return True
                 dic[nums[i]] +=1
             else :
