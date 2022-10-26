261c28d5b902659c8c3301cc7686ca5c222ff3f6
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-10-26 09:36:41 +0900

    Time: 2421 ms (38.72%), Space: 33.3 MB (39.21%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        
+        dic = {}
+        now = 0
+        
+        for i, val in enumerate(nums) :
+            now +=val
+            remain = now%k
+            
+            if remain == 0 and i >= 1 :
+                return True
+            
+            if remain in dic :
+                if i - dic[remain] >= 2 :
+                    return True
+
+            dic[remain] = min(dic.get(remain, i), i)
+        
+        return False
+        
+        
