3be61e2fbba3f544cb9371bea03dd497028390ad
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-11-11 09:36:31 +0900

    Time: 265 ms (16.55%), Space: 15.7 MB (20.60%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        
+        index = 1
+        
+        while index < len(nums): 
+            if nums[index - 1] == nums[index]:
+                nums.pop(index)
+            else:
+                index += 1
+                
+        return index
