348291d90c814fb8d957e307ae9eb6096f7e465e
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2024-02-29 11:11:49 +0900

    Time: 232 ms (54.94%), Space: 37.9 MB (66.05%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isEvenOddTree(self, root: Optional[TreeNode], depth = 0) -> bool:
+        
+        pre_depth = 0
+        pre_value = float('-inf')
+        q = deque()
+        q.append([root, 0])
+        
+        while q :
+            now, depth = q.popleft()
+            
+            if depth%2 == 0 :
+                if now.val %2 == 0 :
+                    return False
+            
+                if depth == pre_depth :
+                    if now.val <= pre_value :
+                        return False
+            
+            elif depth%2 == 1 :
+                if now.val %2 == 1 :
+                    return False
+                if depth == pre_depth :
+                    if now.val >= pre_value :
+                        return False
+            
+            if now.left :
+                q.append([now.left, depth+1])
+            if now.right :
+                q.append([now.right, depth+1])
+            
+            pre_depth = depth
+            pre_value = now.val
+            
+            
+        return True
